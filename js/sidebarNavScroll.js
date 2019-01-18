3cecb9612a22fbaebe531c281cad665e7a922ec7
Tanuja <tanujassawant@gmail.com>
2019-01-17 21:41:14 -0500

    fixed accessible online spelling mistake

---
+++ js/sidebarNavScroll.js
@@ -0,0 +1,17 @@
+
+/*
+$(document).ready(function(){
+
+    $("div").mouseenter(function(){
+        var id = $(this).attr('id');
+        $('a').removeClass('active');
+        $("[href=#"+id+"]").addClass('active');
+    });
+
+});
+
+*/
+
+
+
+
